8d15b3fba28c93d2d45318e0bbbaf0cc1133e1d7
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-06 22:27:00 +0300

    I start new random project
    Its so stupid and boring!

---
+++ Random/Random/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much dicks you want to eat?");
+
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("Horse or monkey?");
+            string input = Console.ReadLine();
+
+            string type;
+            if (input == "horse")
+            {
+                type = "horses";
+            }
+            else if (input == "monkey")
+            {
+                type = "monkeys";
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+            Console.WriteLine($"You will eat {count} {type} dicks!");
+        }
+    }
+}
