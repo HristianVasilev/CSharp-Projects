98176a79f4c5500cf18fccfa09e6aeb541b2cf26
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-13 19:24:42 +0200

    Example -push

---
+++ AutomationTEsting/Example/Example/Classes/Area.cs
@@ -0,0 +1,23 @@
+﻿namespace Example.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Area
+    {
+        public Area(int value)
+        {
+            Value = value;
+        }
+
+        public static Area operator +(Area a1, Area a2)
+        {
+            return new Area(a1.Value + a2.Value);
+        }
+
+        public int Value { get; }
+    }
+}
---
+++ AutomationTEsting/Example/Example/Program.cs
@@ -0,0 +1,9 @@
+﻿using Example.Classes;
+using System.Net.Http.Headers;
+
+Area a1 = new Area(10);
+Area a2 = new Area(20);
+
+Area a3 = a1 + a2;
+
+Console.WriteLine(a3.Value);
